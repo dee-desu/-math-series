f09eaab4b5dddb216ac71457e2005f875eaa1adf
dee-desu <omardarweesh97@outlook.com>
2022-12-16 17:09:55 +0000

    resubmission

--- math_series/series.py
+++ math_series/series.py
@@ -1,23 +1,55 @@
-def fibonacci(nth):
-    if nth == 0 :
+
+'''
+This function takes one parameter
+    and returns the nth value in the fibonacci series.
+'''
+def fibonacci(n):
+    if type(n)!= int:
+        return "please insert only numbers"
+    elif n<0:
+        return "please insert positive numbers"   
+    elif n==0:
         return 0
-    if nth == 1 :
+    elif n==1:
         return 1
     else:
-        return fibonacci(nth-1)+fibonacci(nth-2)
+        return fibonacci(n-1)+fibonacci(n-2)
 
-def lucas(nth):
-    if nth == 0 :
+''' 
+This function takes one parameter and returns the nth value in the lucas series
+'''
+def lucas(n):
+    if type(n)!= int:
+        return "please insert only numbers"
+    elif n<0:
+        return "please insert positive numbers"   
+    elif n==0:
         return 2
-    elif nth == 1 :
+    elif n==1:
         return 1
     else:
-        return lucas(nth-1)+lucas(nth-2)
+        return lucas(n-1)+lucas(n-2)
 
-def sum_series(nth,x=0,y=1):
-    if nth == 0 :
-        return x
-    if nth == 1 :
-        return y
+'''
+that takes one parameter(n) and two optional parameters(n1,n2);
+    if the user inserts only one parameter the function will call fibonacci(n)
+    else if the user inserts three parameter3 and n1=2 and n2=1 the function will call lucas(n)
+    else if the user inserts other values for the optional parameters it will produce other series.
+'''
+def sum_series(n,n1=0,n2=1):
+    if type(n)!= int or type(n1)!= int or type(n2)!= int:
+        return "please insert only numbers"
+    elif n<0:
+        return "please insert positive numbers" 
+    elif n1==0 and n2==1:
+        return fibonacci(n)
+    elif n1==2 and n2==1:
+        return lucas(n)
     else:
-        return sum_series(nth-1,x,y)+sum_series(nth-2,x,y)
+        if n==0:
+            return n1
+        elif n==1:
+            return n2
+        else:
+            return sum_series(n-1,n1,n2)+sum_series(n-2,n1,n2)
+
--- tests/test_Fibonacci_series.py
+++ tests/test_Fibonacci_series.py
@@ -1,50 +1,127 @@
-import pytest 
-from math_series.series import lucas_series, fibonacci_series, sum_series
+from math_series.series import sum_series ,fibonacci,lucas
+import pytest
+#fibonacci testing
+def test_zero_fibonacci():
+    actual = fibonacci(0)
+    expected = 0
+    assert actual == expected
 
-def test_fibonacci_series_one():
-    assert fibonacci_series(0) == 0
-    assert fibonacci_series(1) == 1
-    assert fibonacci_series(2) == 1
-    assert fibonacci_series(10) == 55
-    assert fibonacci_series(-1) == "Please enter a positive integer"
-    assert fibonacci_series("hi") == "Please enter a positive integer"
+def test_one_fibonacci():
+    actual = fibonacci(1)
+    expected = 1
+    assert actual == expected
 
-def test_lucas_series_one():
-    assert lucas_series(0) == 2
-    assert lucas_series(1) == 1
-    assert lucas_series(2) == 3
-    assert lucas_series(10) == 123
-    assert lucas_series(-1) == "Please enter a positive integer"
-    assert lucas_series("hi") == "Please enter a positive integer"
+def test_str_fibonacci():
+    actual=fibonacci("num")
+    expected= "please insert only numbers"
+    assert actual == expected
 
-def test_sum_series_one():
-    assert sum_series(0) == 0
-    assert sum_series(1) == 1
-    assert sum_series(2) == 1
-    assert sum_series(10) == 55
-    assert sum_series(-1) == "Please enter a positive integer"
-    assert sum_series("hi") == "Please enter a positive integer"
-    assert sum_series(0,first_num=0,second_num=1) == 0
-    assert sum_series(1,first_num=0,second_num=1) == 1
-    assert sum_series(2,first_num=0,second_num=1) == 1
-    assert sum_series(10,first_num=0,second_num=1) == 55
-    assert sum_series(-1,first_num=0,second_num=1) == "Please enter a positive integer"
-    assert sum_series("hi",first_num=0,second_num=1) == "Please enter a positive integer"
-    assert sum_series(0,first_num=2,second_num=1) == 2
-    assert sum_series(1,first_num=2,second_num=1) == 1
-    assert sum_series(2,first_num=2,second_num=1) == 3
-    assert sum_series(10,first_num=2,second_num=1) == 123
-    assert sum_series(-1,first_num=2,second_num=1) == "Please enter a positive integer"
-    assert sum_series("hi",first_num=2,second_num=1) == "Please enter a positive integer"
-    assert sum_series(0,first_num=5,second_num=8) == 5
-    assert sum_series(1,first_num=5,second_num=8) == 8
-    assert sum_series(2,first_num=5,second_num=8) == 13
-    assert sum_series(10,first_num=5,second_num=8) == 610
-    assert sum_series(-1,first_num=5,second_num=8) == "Please enter a positive integer"
-    assert sum_series("hi",first_num=5,second_num=8) == "Please enter a positive integer"
-    assert sum_series(0,first_num=-3,second_num=-5) == -3
-    assert sum_series(1,first_num=-3,second_num=-5) == -5
-    assert sum_series(2,first_num=-3,second_num=-5) == -8
-    assert sum_series(10,first_num=-3,second_num=-5) == -377
-    assert sum_series(-1,first_num=-3,second_num=-5) == "Please enter a positive integer"
-    assert sum_series("hi",first_num=-3,second_num=-5) == "Please enter a positive integer"
+def test_four_fibonacci():
+    actual=fibonacci(4)
+    expected= 3
+    assert actual == expected
+
+def test_seven_fibonacci():
+    actual=fibonacci(7)
+    expected= 13
+    assert actual == expected
+
+def test_negative_number_fibonacci():
+    actual=fibonacci(-11)
+    expected= "please insert positive numbers"
+    assert actual == expected
+# lucas testing
+def test_zero_lucas():
+    actual=lucas(0)
+    excepted= 2
+    assert actual == excepted
+def test_one_lucas():
+    actual = lucas(1)
+    expected = 1
+    assert actual == expected
+
+def test_str_lucas():
+    actual=lucas("num")
+    expected= "please insert only numbers"
+    assert actual == expected
+
+def test_four_lucas():
+    actual=lucas(4)
+    expected= 7
+    assert actual == expected
+
+def test_eight_lucas():
+    actual=lucas(8)
+    expected= 47
+    assert actual == expected
+
+def test_negative_number_lucas():
+    actual=lucas(-11)
+    expected= "please insert positive numbers"
+    assert actual == expected
+
+# sum_series testing
+def test_zero():
+    actual=sum_series(0)
+    excepted=0
+    assert actual == excepted
+
+def test_zero_with_options():
+    actual=sum_series(0,2,1)
+    excepted=2
+    assert actual == excepted
+
+def test_one():
+    actual=sum_series(1)
+    excepted=1
+    assert actual == excepted
+
+def test_one_with_options():
+    actual=sum_series(1,2,1)
+    excepted=1
+    assert actual == excepted
+
+def test_str():
+    actual=sum_series("str","str2","str3")
+    excepted="please insert only numbers"
+    assert actual == excepted
+
+def test_two():
+    actual=sum_series(2)
+    excepted=1
+    assert actual == excepted
+
+def test_two_with_options():
+    actual=sum_series(2,2,1)
+    excepted=3
+    assert actual == excepted
+
+def test_zero_other_option1():
+    actual=sum_series(0,0,2)
+    excepted=0
+    assert actual == excepted
+
+def test_zero_other_option2():
+    actual=sum_series(0,2,1)
+    excepted=2
+    assert actual == excepted
+
+def test_three_other_option1():
+    actual=sum_series(3,3,3)
+    excepted=9
+    assert actual == excepted
+
+def test_three_other_option2():
+    actual=sum_series(3,0,3)
+    excepted=6
+    assert actual == excepted
+
+def test_ten_other_option1():
+    actual=sum_series(10,1,0)
+    excepted=34
+    assert actual == excepted
+
+def test_negative_number():
+    actual=sum_series(-11,2,5)
+    expected= "please insert positive numbers"
+    assert actual == expected
